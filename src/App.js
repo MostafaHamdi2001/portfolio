9407d2801002d8d20de8cab511180a1ad0aff2fb
MostafaHamdi2001 <158320404+MostafaHamdi2001@users.noreply.github.com>
2026-07-18 17:56:33 +0300

    profile

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Home/Navbar';
+import Main from './Home/main';
+import Footer from './Home/footer';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/Home/footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function footer() {
+    return (
+        <div>
+            {/* <!-- start Footer --> */}
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="copyright-text-wrap">
+                            <p class="mb-0">
+                                <a rel="sponsored" href="https://github.com/MostafaHamdi2001" target="_blank">reseved by Mostafa Hamdi</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+{/* <!-- end Footer --> */}
+        </div>
+    )
+};
+
+export default footer;
